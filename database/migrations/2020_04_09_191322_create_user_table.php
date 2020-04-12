8692ead185d2c55adff355918b986a578afeb28c
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2020-04-12 13:38:36 -0500

    Please pull these

--- database/migrations/2020_04_09_191322_create_user_table.php
+++ database/migrations/2020_04_09_191322_create_user_table.php
@@ -17,7 +17,7 @@ class CreateUserTable extends Migration
           $table->increments('id');
           $table->string('first_name');
           $table->string('last_name');
-          $table->string('email')->unique();
+          $table->string('email');
           $table->timestamp('email_verified_at')->nullable();
           $table->string('password');
           $table->boolean('is_Admin')->default(0);
